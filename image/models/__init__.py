0020ccc1d19af1922b1aafb2292381a3534c1b58
YueunLee <90474107+YueunLee@users.noreply.github.com>
2026-01-17 20:15:18 +0900

    Delete image directory

--- image/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import Autoencoder
-from .wae import WAE_MMD, WAE_GAN, WAE_GAN2, WAE_KL
--- image/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataset import load_dset
-from .anneal import none_anneal, linear_anneal, exponential_anneal
-from .mmd import mmd_penalty
